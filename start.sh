1600bbdd3baf697cdc45a7a58df33631d81cc558
Mike Ladikov <Ladik1603@gmail.com>
2021-11-23 21:31:08 +0200

    deployment configured

--- start.sh
+++ start.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-# Pull new changes
-git pull
-
-# Prepare Jar
-#mvn clean
-#mvn package
-
 # Ensure, that docker-compose stopped
 docker-compose stop
-
-# Add environment variables
-export BOT_USERNAME=$1
-export BOT_TOKEN=$2
-export BOT_DB_USERNAME=$3
-export BOT_DB_PASSWORD=$4
 
 # Start new deployment
 docker-compose up --build -d
