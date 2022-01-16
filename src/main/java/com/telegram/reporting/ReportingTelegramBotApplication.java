61b2c491e8f3ab876707c047d9726016144a9dcf
miha.ladikov <Ladik1603@gmail.com>
2022-01-16 16:02:14 +0200

    Removed codeqa-analysis

--- src/main/java/com/telegram/reporting/ReportingTelegramBotApplication.java
+++ src/main/java/com/telegram/reporting/ReportingTelegramBotApplication.java
@@ -2,18 +2,13 @@ package com.telegram.reporting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(exclude = { RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class })
-@EntityScan("com.telegram.reporting.*")
+@SpringBootApplication
 public class ReportingTelegramBotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReportingTelegramBotApplication.class, args);
     }
-
 }
