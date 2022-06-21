4e6bf00723678af1090eb2c5b0cf20e3671eaa3e
Serhii_Markovskyi <markovsky.sergey@gmail.com>
2022-06-21 20:52:45 +0300

    added validation to user date and time input

--- src/main/java/com/telegram/reporting/service/impl/GuardServiceImpl.java
+++ src/main/java/com/telegram/reporting/service/impl/GuardServiceImpl.java
@@ -9,9 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.statemachine.StateContext;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 @Slf4j
 @Service
 public class GuardServiceImpl implements GuardService {
@@ -24,15 +21,23 @@ public class GuardServiceImpl implements GuardService {
 
     @Override
     public <S, E> boolean validateDate(StateContext<S, E> context) {
+        var regexDay = "^([1-9]|0[1-9]|1\\d|2\\d|3[01])$";
+        var regexDayMonth = "^([1-9]|0[1-9]|1\\d|2\\d|3[01]).(0[1-9]|1[0-2])$";
+        var regexFullDate = "^([1-9]|0[1-9]|1\\d|2\\d|3[01]).(0[1-9]|1[0-2]).(19|20)\\d{2}$";
 
-        String chatId = TelegramUtils.currentChatId(context);
-        String userInput = (String) context.getExtendedState().getVariables().get(ContextVariable.DATE);
+        var chatId = TelegramUtils.currentChatId(context);
+        var userInput = (String) context.getExtendedState().getVariables().get(ContextVariable.DATE);
 
-        if (userInput.matches(".*\\d+.*")) {
+        if (userInput.matches(regexDay) || userInput.matches(regexDayMonth) || userInput.matches(regexFullDate)) {
             return true;
         }
-        String dateExample = LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        sendBotMessageService.sendMessage(chatId, "Вы не верно ввели дату. Попробуйте еще раз в формате - " + dateExample);
+
+        var dateErrorMessage = """
+                Вы не верно ввели дату.
+                Попробуйте еще раз в формате - 29.08.1997.
+                Также допустимо - 29 или 29.08
+                """;
+        sendBotMessageService.sendMessage(chatId, dateErrorMessage);
 
         return false;
     }
@@ -42,11 +47,15 @@ public class GuardServiceImpl implements GuardService {
         String chatId = TelegramUtils.currentChatId(context);
         String userInput = (String) context.getExtendedState().getVariables().get(ContextVariable.REPORT_TIME);
 
-        if (userInput.matches("\\d+") && Integer.parseInt(userInput) > 0) {
+        if (userInput.matches("\\d+") && isWithinDay(userInput)) {
             return true;
         }
 
-        sendBotMessageService.sendMessage(chatId, "Вы не верно ввели время. Допустимые значения - положительные числа");
+        var timeErrorMessage = """
+                Вы не верно ввели время.
+                Допустимые значения - положительные числа в пределах 24 часов
+                """;
+        sendBotMessageService.sendMessage(chatId, timeErrorMessage);
         return false;
     }
 
@@ -62,7 +71,17 @@ public class GuardServiceImpl implements GuardService {
             return true;
         }
 
-        sendBotMessageService.sendMessage(chatId, "Ваше примечание слишком лаконичное)) попробуйте написать больше БУКАВ)) Минимальное количество - " + minNoteLength);
+        var noteErrorMessage = """
+                Ваше примечание слишком лаконичное))
+                попробуйте написать больше БУКАВ))
+                Минимальное количество ->""" + minNoteLength;
+
+        sendBotMessageService.sendMessage(chatId, noteErrorMessage);
         return false;
+    }
+
+    private boolean isWithinDay(String userInput) {
+        var hours = Integer.parseInt(userInput);
+        return hours > 0 && hours <= 24;
     }
 }
