014df16741839ae9fd62f7f550af1ffd05d3cf16
Serhii_Markovskyi <markovsky.sergey@gmail.com>
2022-06-22 15:08:08 +0300

    added @JoinColumn to Report entity for explicit mapping of relation

--- src/main/java/com/telegram/reporting/repository/entity/Report.java
+++ src/main/java/com/telegram/reporting/repository/entity/Report.java
@@ -4,16 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -32,6 +23,7 @@ public class Report {
     private LocalDate date;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ToString.Exclude
