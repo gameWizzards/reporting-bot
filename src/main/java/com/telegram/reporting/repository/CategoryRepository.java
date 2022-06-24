665e84211bdf67f3f831e475d63203e465588138
Serhii_Markovskyi <markovsky.sergey@gmail.com>
2022-06-24 22:20:35 +0300

    added JPQL queries to User & Category repos

--- src/main/java/com/telegram/reporting/repository/CategoryRepository.java
+++ src/main/java/com/telegram/reporting/repository/CategoryRepository.java
@@ -2,10 +2,12 @@ package com.telegram.reporting.repository;
 
 import com.telegram.reporting.repository.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    @Query(value = "SELECT c FROM Category c WHERE c.name=?1")
     Category getByName(String name);
 }
--- src/main/java/com/telegram/reporting/repository/UserRepository.java
+++ src/main/java/com/telegram/reporting/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.telegram.reporting.repository;
 
 import com.telegram.reporting.repository.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,7 +13,9 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByPhone(String phoneNumber);
+    @Query(value = "SELECT u FROM User u WHERE u.phone=?1")
+    User findByPhone(String phone);
 
+    @Query(value = "SELECT u FROM User u WHERE u.chatId=?1")
     User findByChatId(Long chatId);
 }
